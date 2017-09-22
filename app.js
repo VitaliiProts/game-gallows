0ac4bd39280c01ea654e8aa2c1b87e39c7013b8d
Vitalii Prots <vitalikprocj@gmail.com>
2017-09-23 01:11:50 +0300

    add else if(guess.length < !==1) {

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var words = ["зошит", "ручка", "голуб", "пляцок"]; //Створили масив
+var words = ["зошит", "ручка", "голуб", "селфі", "дедлайн"]; //Створили масив
 var	word = words[Math.floor(Math.random() * words.length)]; // Вибрали випадкове слово
 var answerArray = [];
 
 for (var i = 0; i < word.length; i++) {
-	answerArray[i] = "_"
+	answerArray[i] = "*";
 }
 
 var remainingLetters = word.length;
@@ -11,12 +11,14 @@ var remainingLetters = word.length;
 // Цикл
 while (remainingLetters > 0) {
 	// Показуєм стан гри
-	alert(answerArray.join(" "));
+	alert(answerArray.join(""));
 	// Запитуєм варіант відповіді
 	var guess = prompt("Відгадайте букву, або натисніть \'Отмена\', для виходу з гри.");
 	if (guess === null) {
 		// Виходим з ігрового циклу
 		break;
+	}else if(guess.length < !==1) {
+		alert("Будь ласка введіть тільки 1 букву.")
 	} else {
 		// Обновлюєм стан гри
 		for (var j = 0; j < word.length; j++) {
